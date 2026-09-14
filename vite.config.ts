17cb3ea77a778fece7e9e31cbcd691a65478a520
tomasgajardomutis <tomas.gajardo.mutis@gmail.com>
2026-09-14 17:25:06 -0300

    Configurar plugin oficial de Cloudflare para Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
+import { cloudflare } from '@cloudflare/vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [cloudflare()],
   base: './',
   build: {
-    outDir: process.env.APPDEPLOY_VITE_OUT_DIR || 'dist',
-    sourcemap:
-      process.env.APPDEPLOY_VITE_SOURCEMAP === 'hidden' ? 'hidden' : false,
+    outDir: 'dist',
+    sourcemap: false,
     rollupOptions: {
       maxParallelFileOps: 128,
     },
